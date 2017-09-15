9fc53a58e112f9428c202a0cef6b9b3fbd792631
Mark Wayne <mwpulido24@gmail.com>
2017-09-15 16:48:51 +0800

    Added Treatment Records on pages

---
+++ src/pages/TreatmentRecords/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PatientInfo extends Component{
+    render(){
+        return(
+            <div>
+                Treatment Records!
+            </div>            
+        );
+    }
+}
+
+export default PatientInfo;
