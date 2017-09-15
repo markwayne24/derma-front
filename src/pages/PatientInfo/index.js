24a139249f76d6739afd53d7fd53e0114ec25f42
Mark Wayne <mwpulido24@gmail.com>
2017-09-15 18:25:56 +0800

    Added Form sample for Patient information on pages

--- src/pages/PatientInfo/index.js
+++ src/pages/PatientInfo/index.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
+import { Form, Input, Row, Col } from 'antd';
+const FormItem = Form.Item;
 
 class PatientInfo extends Component{
     render(){
         return(
             <div>
-                Patient information!
+                <Form onSubmit={this.handleSubmit}>
+                    <FormItem
+                    hasFeedback
+                    >
+                        <Row gutter={20}>
+                            <Col xs={24} xs={12} lg={8}>
+                                <Input/>
+                            </Col>
+                            <Col xs={24} xs={12} lg={8}>
+                                <Input />
+                            </Col>
+                            <Col xs={24} xs={12} lg={8}>
+                                <Input />
+                            </Col>
+                        </Row>
+                    </FormItem>
+                    <FormItem
+                    hasFeedback
+                    >
+                        <Row gutter={20}>
+                            <Col xs={24} xs={12} lg={8}>
+                                <Input/>
+                            </Col>
+                            <Col xs={24} xs={12} lg={8}>
+                                <Input />
+                            </Col>
+                            <Col xs={24} xs={12} lg={8}>
+                                <Input />
+                            </Col>
+                        </Row>
+                    </FormItem>
+                </Form>
             </div>            
         );
     }
