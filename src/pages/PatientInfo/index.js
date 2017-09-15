c2b2256a1eae0c1884dd2d326e9109dc13a8fde9
Mark Wayne <mwpulido24@gmail.com>
2017-09-15 16:46:53 +0800

    Added Patient information on pages

---
+++ src/pages/PatientInfo/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PatientInfo extends Component{
+    render(){
+        return(
+            <div>
+                Patient information!
+            </div>            
+        );
+    }
+}
+
+export default PatientInfo;
