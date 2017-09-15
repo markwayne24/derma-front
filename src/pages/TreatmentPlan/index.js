986465f1c0ca21bf54b566c20c9f3ffdc98ab251
Mark Wayne <mwpulido24@gmail.com>
2017-09-15 16:48:35 +0800

    Added Treatment Plan on pages

---
+++ src/pages/TreatmentPlan/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PatientInfo extends Component{
+    render(){
+        return(
+            <div>
+                Treatment Plan!
+            </div>            
+        );
+    }
+}
+
+export default PatientInfo;
